9def935346c5b04a01ab416e90ed45373d404e04
Jordan McCrank <81048903+InsertFunny@users.noreply.github.com>
2022-03-10 23:00:01 +0000

    Made some minor changes

--- part3.py
+++ part3.py
@@ -1,8 +1,8 @@
 import math
 
-def say_math(math_func, num):
+def say_math(math_func, num, func):
   result = math_func(num)
-  print("The result of " + str(math_func) + " applied to " + str(num) + " is " + str(result))
-say_math(math.sqrt, 9)
-say_math(math.modf, 100)
-say_math(math.exp, 24)
+  print("The result of " + func + " applied to " + str(num) + " is " + str(result))
+say_math(math.sqrt, 9,"Square root")
+say_math(math.modf, 100, "Mod F")
+say_math(math.exp, 24, "EXP")
